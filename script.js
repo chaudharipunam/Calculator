8087b3c3a07b91ef5aa77d81dc4d69ed0d1c377f
chaudharipunam <punamchaudhari128@gmail.com>
2024-07-27 22:26:44 +0530

    calculator project modify

--- script.js
+++ script.js
@@ -6,16 +6,6 @@ let display=document.getElementById("display");
             display.innerText=input.slice(-10);
         }
         
-        function factorial(number){
-            if(number<0)return "Error";
-            if(number===0||number===1)return 1;
-            let res=1;
-            for (let i=2; i<=number; i++){
-                res*=i;
-            }
-            return res;
-        }
-
         buttons.map((btn)=>{
             btn.addEventListener("click",(e)=>{
                 let key=e.target.innerText;
@@ -34,19 +24,6 @@ let display=document.getElementById("display");
                 else if(key==="del"){
                     input=input.slice(0,-1);
                 }
-                else if(key==="sqrt"){
-                   input=Math.sqrt(parseFloat(input)).toString();
-                }
-                else if(key==="exp"){
-                   input+="**";
-                }
-                else if (key==="sq"){
-                    input=Math.pow(parseFloat(input), 2).toString();
-                } 
-                else if(key==="1/x"){
-                    let number = parseInt(input);
-                    input = factorial(number).toString();
-                } 
                 else{
                     if(input==="0"&&key!=="."){
                         input=key;
