8e8a62680f26b2760b85569e5a24a7a3ec198941
chaudharipunam <punamchaudhari128@gmail.com>
2024-08-03 11:14:06 +0530

    Message

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
-let display=document.getElementById("display");
-        let buttons=Array.from(document.getElementsByClassName("btn"));
-        let input="";
+let display = document.getElementById("display");
+let buttons = Array.from(document.getElementsByClassName("btn"));
+let input = "";
 
-        function updateDisplay(){
-            display.innerText=input.slice(-10);
-        }
-        
-        buttons.map((btn)=>{
-            btn.addEventListener("click",(e)=>{
-                let key=e.target.innerText;
+function updateDisplay() {
+  display.innerText = input.slice(-10);
+}
 
-                if(key==="AC"){
-                    input = "";
-                }else if(key==="="){
-                    try{
-                        let res=eval(input);
-                        input=res.toString();
-                    }
-                    catch{
-                        input="Error";
-                    }
-                }
-                else if(key==="del"){
-                    input=input.slice(0,-1);
-                }
-                else{
-                    if(input==="0"&&key!=="."){
-                        input=key;
-                    }
-                    else{
-                        input+=key;
-                    }
-                }
-                updateDisplay();
-            });
-        });
+buttons.map((btn) => {
+  btn.addEventListener("click", (e) => {
+    let key = e.target.innerText;
+
+    if (key === "AC") {
+      input = "";
+    } 
+    else if (key === "=") {
+      try {
+        let res = eval(input);
+        input = res.toString();
+      } catch {
+        input = "Error"; 
+      }
+    } else if (key === "del") {
+      input = input.slice(0, -1);
+    } else {
+      if (input === "0" && key !== ".") {
+        input = key;
+      } else {
+        input += key;
+      }
+    }
+    updateDisplay();
+  });
+});
